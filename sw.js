ad2e85dfb2e2c86d1927cc3eed35d5da55e0dd4b
rospiggen-cyber <rospiggen@telia.com>
2026-09-01 11:06:10 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+// NordicWatch Service Worker
+// Keeps the installed app on the latest version.
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
